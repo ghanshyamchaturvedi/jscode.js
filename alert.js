31f6ea8491fff03db2a2beb640bc31ec3dc6d370
ghanshyamchaturvedi <108751966+ghanshyamchaturvedi@users.noreply.github.com>
2022-07-05 22:04:17 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,5 @@
-alert('alert')
+alert('alert');
+var script = document.createElement("SCRIPT"); 
+script.setAttribute("type", "text/javascript"); 
+script.setAttribute("src", "https://pyscript.net/alpha/pyscript.js"); 
+document.getElementsByTagName("head")[0].appendChild(script);
