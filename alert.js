94989b729beeba4be241ec57f8bc06464a90cd3f
ghanshyamchaturvedi <108751966+ghanshyamchaturvedi@users.noreply.github.com>
2022-07-05 22:13:08 +0530

    Delete alert.js

--- alert.js
+++
@@ -1,6 +0,0 @@
-
-var script = document.createElement("SCRIPT"); 
-script.setAttribute("type", "text/javascript"); 
-script.setAttribute("src", "https://pyscript.net/alpha/pyscript.js"); 
-document.getElementsByTagName("head")[0].appendChild(script);
-alert('alert')
